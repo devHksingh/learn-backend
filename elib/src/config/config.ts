47c49ad7f5f0f93d005d749812c3f7ffe296be28
devHksingh <hksingh.dev@gmail.com>
2024-04-11 00:15:22 +0530

    add MongoDB and mongoose setup

--- elib/src/config/config.ts
+++ elib/src/config/config.ts
@@ -3,7 +3,8 @@ import { config as conf } from "dotenv"
 conf()
 
 const _config = {
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    databaseUrl: process.env.MONGODB_URL,
 }
 
 export const config  = Object.freeze(_config) //read only
