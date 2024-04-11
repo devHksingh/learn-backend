39b30a14a537c639531917d6f86763d1d62004e2
devHksingh <hksingh.dev@gmail.com>
2024-04-11 20:06:05 +0530

    create user router

--- elib/src/app.ts
+++ elib/src/app.ts
@@ -4,6 +4,7 @@ import express, { NextFunction, Request, Response } from 'express'
 import createHttpError, { HttpError } from 'http-errors'
 import { config } from './config/config'
 import globalErrorHandler from './middlewares/globalErrorHanlder'
+import userRouter from './user/userRouter'
 
 const app = express()
 
@@ -15,6 +16,10 @@ app.get('/',(req,res,next)=>{
     // throw error
     res.json({message:"Welcome to elib"})
 })
+
+// user router
+
+app.use('/api/users',userRouter)
 
 
 //  Global error handler
