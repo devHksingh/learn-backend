82fa97885c055812285f448bf2cab62b7d688fca
devHksingh <hksingh.dev@gmail.com>
2024-04-20 13:35:28 +0530

    add global error handler

--- dummy-Server/src/app.ts
+++ dummy-Server/src/app.ts
@@ -1,4 +1,6 @@
-import express  from "express";
+import express, { NextFunction, Request, Response }  from "express";
+
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express()
 
@@ -6,10 +8,18 @@ const app = express()
 // app.use()
 
 app.get('/',(req,res,next)=>{
+
+    // const error = createHttpError(400,"something went wrong testing error!")
+    // throw error
     res.json({
         message:'Wellcome to dummy server api '
     })
 })
+
+// Global error handler
+
+app.use(globalErrorHandler)
+
 
 
 export default app
