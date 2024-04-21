990272c40e47cba4e0282b97c9a5f9eaaac44182
devHksingh <hksingh.dev@gmail.com>
2024-04-21 22:02:10 +0530

    add hash function

--- dummy-Server/src/user/userController.ts
+++ dummy-Server/src/user/userController.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
+import bcrypt from 'bcryptjs'
 
 
 const createUser = async (req:Request,res:Response,next:NextFunction)=>{
@@ -7,14 +9,27 @@ const createUser = async (req:Request,res:Response,next:NextFunction)=>{
     // return res.json({req:req.body})
     
 
-    // const {name,email,password} = req.body
+    const {name,email,password} = req.body
     //1. validation
     // if(!name || !email || !password){
     //     const error = createHttpError(400,'All fields are required')
     //     return next(error)
     // }
     // 2.logic/process
-    // 
+    // DB call
+    const user = await userModel.findOne({email:email})
+
+    if(user){
+        const error = createHttpError(400,"User already exist with this email ID")
+        return next(error)
+    }
+
+    // user store in db
+    // user password -> hash
+    const hashedPassword = await bcrypt.hash(password,10)
+    
+
+
     // 3.Response
 
     return res
