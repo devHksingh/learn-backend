93d88dd6b08779cfc9eb757e28947960ab23d07d
devHksingh <hksingh.dev@gmail.com>
2024-04-22 20:21:52 +0530

    add error handling  and add genrateAccessToken function

--- dummy-Server/src/user/userController.ts
+++ dummy-Server/src/user/userController.ts
@@ -3,9 +3,26 @@ import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from 'bcryptjs'
 import { sign } from "jsonwebtoken";
-import crypto from 'crypto'
 import { config } from "../config/config";
 
+
+
+const genrateAccessToken = async(userId:string)=>{
+    
+    try {
+        const user = await userModel.findById(userId)
+        
+        const token = await sign({sub:user?._id},config.jwtSecret as string,{
+            expiresIn:'1D',
+            algorithm:"HS256"
+        })
+
+        return token
+        
+    } catch (err) {
+            throw new Error('Something went wrong while generating token')
+    }
+}
 
 const createUser = async (req:Request,res:Response,next:NextFunction)=>{
     // console.log("request data" , req.body);
@@ -27,35 +44,57 @@ const createUser = async (req:Request,res:Response,next:NextFunction)=>{
         return next(error)
     }
 
-    // user store in db
-    // user password -> hash
-    const hashedPassword = await bcrypt.hash(password,10)
-
-    if(!hashedPassword){
-        const error = createHttpError(400,'Failed to create hashed password . try it again!!')
-        return next(error)
-    }
-    const newUser = await userModel.create({
-        name,
-        email,
-        password:hashedPassword
-    })
-    if(!newUser){
-        const error = createHttpError(400,'Failed to create user in DB . try it again!!')
-        return next(error)
-    }
-    // token generation :JWT
-    const token = sign({sub:newUser._id},config.jwtSecret as string,{expiresIn:'7d',algorithm:"HS256"})
-
     
-    // 3.Response
 
-    return res
-    .status(201)
-    .json({
-        messgae:`User created and id is ${newUser._id} `,
-        token:token
-    })
+    try {
+        // user store in db
+        // user password -> hash
+        const hashedPassword = await bcrypt.hash(password,10)
+
+        if(!hashedPassword){
+            const error = createHttpError(400,'Failed to create hashed password . try it again!!')
+            return next(error)
+        }
+        const newUser = await userModel.create({
+            name,
+            email,
+            password:hashedPassword
+        })
+        if(!newUser){
+            const error = createHttpError(400,'Failed to create user in DB . try it again!!')
+            return next(error)
+        }
+        // token generation :JWT
+        // const token = sign({sub:newUser._id},config.jwtSecret as string,{expiresIn:'7d',algorithm:"HS256"})
+        // 3.Response
+        try {
+            const token = await genrateAccessToken(newUser._id)
+    
+            return res
+            .status(201)
+            .json({
+                messgae:`User created and id is ${newUser._id} `,
+                token:token
+            })
+        } catch (error) {
+            
+            
+            try {
+                await userModel.deleteOne({_id:newUser._id})
+            } catch (error) {
+                return next(createHttpError(500, `Failed to delete user. User Id is ${newUser._id}`))
+            }
+            
+            return next(createHttpError(500,`${error}`))
+        } 
+        
+    } catch (err) {
+        
+        console.log(err);
+        return next(createHttpError(500,`${err}`))
+        
+    }
+    
 }
 
 const loginUser = async (req:Request,res:Response,next:NextFunction) =>{
@@ -76,12 +115,21 @@ const loginUser = async (req:Request,res:Response,next:NextFunction) =>{
 
     // create accessToken
 
-    const token = sign({sub:user._id},config.jwtSecret as string,{
-        expiresIn:'7d',
-        algorithm:"HS256"
-    })
+    // const token = sign({sub:user._id},config.jwtSecret as string,{
+    //     expiresIn:'7d',
+    //     algorithm:"HS256"
+    // })
 
-    res.json({access_Token: token})
+    try {
+        const token = await genrateAccessToken(user._id)
+
+        res.json({access_Token: token})
+
+    } catch (error) {
+        return next(createHttpError(500,`${error}`))
+    }
+
+    
 }
 
 export {createUser,loginUser}
