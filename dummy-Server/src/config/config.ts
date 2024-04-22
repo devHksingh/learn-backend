848d5b5cc6f77bbb1fbe600d140d97865e076597
devHksingh <hksingh.dev@gmail.com>
2024-04-22 21:34:28 +0530

    add cors

--- dummy-Server/src/config/config.ts
+++ dummy-Server/src/config/config.ts
@@ -5,7 +5,8 @@ const _config ={
     port : process.env.PORT,
     dbUrl: process.env.MONGO_CONNECTION_STRING,
     env:process.env.NODE_ENV,
-    jwtSecret:process.env.JWT_SECRET
+    jwtSecret:process.env.JWT_SECRET,
+    frontendDomain:process.env.FORNTEND_DOMAIN,
 }
 
 export const config = Object.freeze(_config)
