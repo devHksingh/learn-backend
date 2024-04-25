5ac8a8b4fe246e6ecd5824459d235146b557f2f3
devHksingh <hksingh.dev@gmail.com>
2024-04-25 19:17:44 +0530

    fix get single book endpoint "author.name"

--- elib/src/book/bookController.ts
+++ elib/src/book/bookController.ts
@@ -180,7 +180,7 @@ const getSingleBook = async (req: Request, res: Response, next: NextFunction)=>{
   const bookId = req.params.bookId
   try {
 
-    const book = await bookModel.findOne({_id:bookId})
+    const book = await bookModel.findOne({_id:bookId}).populate("author","name")
 
     if(!book){
       return next(createHttpError(404,"Book not found"))
