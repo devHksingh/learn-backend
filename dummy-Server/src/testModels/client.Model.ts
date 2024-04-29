7c0100ae9a0eb4a585599a7592738ddecd99c079
devHksingh <hksingh.dev@gmail.com>
2024-04-29 22:42:14 +0530

    add client model

---
+++ dummy-Server/src/testModels/client.Model.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const clientSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+    },
+    phoneNumber:{
+        type:Number,
+        required:true,
+        unique:true
+    },
+    avatar:{
+        type:String, //cloudinary url
+        
+    },
+    password:{
+        type:String,
+        required: [true,'Password is required']
+
+    },
+    refreshToken:{
+        type:String
+    },
+    taskHistory:{
+        
+    },
+    
+},{timestamps:true})
+
+export const Client = mongoose.model('Client',clientSchema)
