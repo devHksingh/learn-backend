cccbb461ead306acab3e97a17df0f6426a116149
devHksingh <hksingh.dev@gmail.com>
2024-05-01 12:22:29 +0530

    fix minor typo

--- dummy-Server/src/user/user_login_schema.ts
+++ dummy-Server/src/user/user_login_schema.ts
@@ -3,7 +3,7 @@ import { body } from "express-validator";
 const schema = [
     body('email')
         .isEmail()
-        .withMessage("Email is required fro login"),
+        .withMessage("Email is required for login"),
     body('password')
         .isString()
         .withMessage('password is required for login and must be at least 4 characters long')
