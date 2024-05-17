0ee34372040866fbd7024c52c0113813c05af2a2
devHksingh <hksingh.dev@gmail.com>
2024-05-17 16:44:52 +0530

    add express prettier

--- Paytm_Backend_Part1/server.ts
+++ Paytm_Backend_Part1/server.ts
@@ -1,1 +1,14 @@
-console.log("HI");
+import app from "./src/app";
+
+const startServer = async () => {
+  const port = 4000;
+  try {
+    app.listen(port, () => {
+      console.log(`Server is Runing on ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
