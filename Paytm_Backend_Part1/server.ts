2f81e80462bfc07d43316b9e4be1325063e36e09
devHksingh <hksingh.dev@gmail.com>
2024-05-17 21:25:15 +0530

    add user schema

--- Paytm_Backend_Part1/server.ts
+++ Paytm_Backend_Part1/server.ts
@@ -1,9 +1,11 @@
 import app from "./src/app";
 import { Config } from "./src/config";
+import connectDB from "./src/config/db";
 
 const startServer = async () => {
   const port = Config.port;
   try {
+    await connectDB()
     app.listen(port, () => {
       console.log(`Server is Runing on ${port}`);
     });
---
+++ Paytm_Backend_Part1/src/users/user.Model.ts
@@ -0,0 +1,38 @@
+import mongoose,{Schema} from "mongoose";
+
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+},{timestamps:true})
