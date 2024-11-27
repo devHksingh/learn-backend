021c4cbb90e28c8752352de9e32dc804b9b42c15
devHksingh <hksingh.dev@gmail.com>
2024-11-27 22:49:36 +0530

    test lint staged

--- test_backendSteup/auth-service/src/server.ts
+++ test_backendSteup/auth-service/src/server.ts
@@ -1,7 +1,12 @@
 function welcome(name: string) {
   console.log("Welcome " + name);
+  const user ={
+    name:"harshit"
+  }
+
+  console.log(user["name"]);
+  
 }
 console.log('testing husky setup ')
-console.log('testing husky setup ')
-console.log('testing husky setup ')
+
 welcome("harshit");
