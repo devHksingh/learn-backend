75e94468bedc23d070f0767325b75a977febb12b
devHksingh <hksingh.dev@gmail.com>
2024-11-29 09:36:16 +0530

    Implementing Error Handling

--- test_backendSteup/auth-service/src/app.ts
+++ test_backendSteup/auth-service/src/app.ts
@@ -1,4 +1,6 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response, NextFunction } from "express"
+import logger from "./config/logger";
+import  { HttpError } from "http-errors";
 
 const app = express()
 
@@ -8,7 +10,28 @@ app.use(express.json())
 //     res.sendStatus(200).json({ message: 'Welcome to home page' })
 // })
 app.get('/', (req: Request, res: Response) => {
+    // const err = createHttpError(401, "this route is not public")
+    // next(err)
     res.status(200).json({ message: 'Welcome to home page' });
 });
+
+// global error handler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message)
+    const statusCode = err.statusCode || 500
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: '',
+                location: ''
+            }
+        ]
+    })
+})
+
 
 export default app
