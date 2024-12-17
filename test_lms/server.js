fc97bd14bf819d334ac1f910cef8896fcbf413d9
devHksingh <hksingh.dev@gmail.com>
2024-12-17 18:01:35 +0530

    add helmet

--- test_lms/server.js
+++ test_lms/server.js
@@ -1,50 +1,52 @@
 import { config } from "dotenv";
 import express from "express";
 import morgan from "morgan";
-import rateLimit from 'express-rate-limit'
+import rateLimit from "express-rate-limit";
 config();
+import helmet from "helmet";
 
 const app = express();
 const PORT = process.env.PORT;
 
 // handeling global rate limiter
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-    message:"Too many request from this IP ,please try later"
-})
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  message: "Too many request from this IP ,please try later",
+});
 // security middleware
-
-// logging middleware 
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
+app.use(helmet());
+app.use("/api", limiter);
+// logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // API Routes
 
 // Global Error handler
-app.use((err,req,res,next)=>{
-    console.error(err.stack)
-    res.status(err.status || 500).json({
-        status:'error',
-        message:err.message|| 'Interval server error',
-        ...(process.env.NODE_ENV ==='development' && {stack:err.stack})
-    })
-})
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    status: "error",
+    message: err.message || "Interval server error",
+    ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
+  });
+});
 
 // body parser middelware
-app.use(express.json({limit:'10kb'}))
-app.use(express.urlencoded({extended:true,limit:"10kb"}))
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
 // it should be always at bottom
 // 404 handeler
 
-app.use((req,res)=>{
-    res.status(404).json({
-        status:"error",
-        message:"Route not found"
-    })
-})
+app.use((req, res) => {
+  res.status(404).json({
+    status: "error",
+    message: "Route not found",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT} in ${process.env.NODE_ENV}`);
