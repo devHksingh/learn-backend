693652d973a21732775b38106af5a29f915b9c16
devHksingh <hksingh.dev@gmail.com>
2024-12-17 18:55:44 +0530

    restructure

--- test_lms/server.js
+++ test_lms/server.js
@@ -23,39 +23,48 @@ app.use("/api", limiter);
 app.use(mongoSanitize());
 app.use(hpp())
 
-// cookieParser
-app.use(cookieParser())
+
 
 // logging middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-// API Routes
 
-// Global Error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(err.status || 500).json({
-    status: "error",
-    message: err.message || "Interval server error",
-    ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
-  });
-});
 
 // body parser middelware
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+// cookieParser
+app.use(cookieParser())
+
+
+
+
+// API Routes
+
+
+
 
 // it should be always at bottom
 // 404 handeler
 
 app.use((req, res) => {
-  res.status(404).json({
-    status: "error",
-    message: "Route not found",
+    res.status(404).json({
+      status: "error",
+      message: "Route not found",
+    });
   });
-});
+
+// Global Error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).json({
+      status: "error",
+      message: err.message || "Interval server error",
+      ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
+    });
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT} in ${process.env.NODE_ENV}`);
