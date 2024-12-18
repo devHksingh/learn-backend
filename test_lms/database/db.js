81a1952bcb50472fcb92fffb99ee1ef21001bd8f
devHksingh <hksingh.dev@gmail.com>
2024-12-18 15:38:38 +0530

    add handleConnectionError

--- test_lms/database/db.js
+++ test_lms/database/db.js
@@ -27,22 +27,48 @@ class DatabaseConnection {
 
   //   connection
   async connect() {
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO db URI is not defined in env variables");
-    }
-    const connectionOptions = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      maxPoolSize: 5,
-      serverSelectionTimeoutMS: 5000,
-      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      family: 4, // Use IPv4
-    };
-    if (process.env.NODE_ENV === "development") {
-      mongoose.set("debug", true);
-    }
+    try {
+      if (!process.env.MONGO_URI) {
+        throw new Error("MONGO db URI is not defined in env variables");
+      }
+      const connectionOptions = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        maxPoolSize: 5,
+        serverSelectionTimeoutMS: 5000,
+        socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+        family: 4, // Use IPv4
+      };
+      if (process.env.NODE_ENV === "development") {
+        mongoose.set("debug", true);
+      }
 
-    await mongoose.connect(process.env.MONGO_URI, connectionOptions);
-    this.retryCount = 0; // rest retry count on success
+      await mongoose.connect(process.env.MONGO_URI, connectionOptions);
+      this.retryCount = 0; // reset retry count on success
+    } catch (error) {
+      console.error("Failed to connect to MongoDB:", error.message);
+      await this.handleConnectionError();
+    }
   }
+  async handleConnectionError() {
+    if (this.retryCount < MAX_RETRIES) {
+      this.retryCount++;
+      console.log(
+        `Retrying connection ....... Attempt ${this.retryCount} of ${MAX_RETRIES}`
+      );
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, RETRY_INTERVAL);
+      });
+      return this.connect();
+    } else {
+      console.error(
+        `Failed to connect to MONGODB AFTER ${MAX_RETRIES} attempts`
+      );
+      process.exit(1);
+    }
+  }
+  
+
 }
