062f37dca9754a4e079289b007f94e3f33ee2974
devHksingh <hksingh.dev@gmail.com>
2024-12-18 17:54:32 +0530

    add hooks and method for password

--- test_lms/models/user.model.js
+++ test_lms/models/user.model.js
@@ -3,80 +3,101 @@ import bcrypt from "bcryptjs";
 import crypto from "crypto";
 import { match } from "assert";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"],
-    trim: true,
-    maxLength: [50, "Name cannot exceed 50 characters"],
-  },
-
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    trim: true,
-    unique: true,
-
-    lowercase: true,
-    match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Please provide a valid email"],
-  },
-
-  password: {
-    type: String,
-    required: [true, "password is required"],
-    minLength: [6, "Password must be at least 8 char"],
-    select: false,
-  },
-
-  role: {
-    type: String,
-    enum: {
-      values: ["student", "instructor", "admin"],
-      message: "Please select a valid role",
-      default: "student",
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      maxLength: [50, "Name cannot exceed 50 characters"],
     },
-  },
-  avatar: {
-    type: String,
-    default: "defaut-avatar.png",
-  },
 
-  bio: {
-    type: String,
-    maxLength: [200, "Bio cannot exceed 200 characters"],
-  },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      unique: true,
 
-  enrolledCourses: [
-    {
-      course: {
+      lowercase: true,
+      match: [
+        /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+        "Please provide a valid email",
+      ],
+    },
+
+    password: {
+      type: String,
+      required: [true, "password is required"],
+      minLength: [6, "Password must be at least 8 char"],
+      select: false,
+    },
+
+    role: {
+      type: String,
+      enum: {
+        values: ["student", "instructor", "admin"],
+        message: "Please select a valid role",
+        default: "student",
+      },
+    },
+    avatar: {
+      type: String,
+      default: "defaut-avatar.png",
+    },
+
+    bio: {
+      type: String,
+      maxLength: [200, "Bio cannot exceed 200 characters"],
+    },
+
+    enrolledCourses: [
+      {
+        course: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Course",
+        },
+        enrolledAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    createdCourse: [
+      {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Course",
       },
-      enrolledAt: {
-        type: Date,
-        default: Date.now,
-      },
-    },
-  ],
-  createdCourse: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Course",
-    },
-  ],
+    ],
 
-  resetPasswordToken: {
-    type: String,
+    resetPasswordToken: {
+      type: String,
+    },
+    resetPasswordExpire: {
+      type: Date,
+    },
+    lastActive: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  resetPasswordExpire: {
-    type: Date,
-  },
-  lastActive: {
-    type: Date,
-    default: Date.now,
-  },
-},{
-    timestamps:true
+  {
+    timestamps: true,
+  }
+);
+
+// hashing password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 11);
+  next();
 });
+
+// compare password
+
+userSchema.methods.comparePassword = async function (enterPassword) {
+  return await bcrypt.compare(enterPassword, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
