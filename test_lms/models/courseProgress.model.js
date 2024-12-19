ebf99114abf0883c5f902c94a3c4bb3dcca95dbd
devHksingh <hksingh.dev@gmail.com>
2024-12-19 12:10:38 +0530

    add lectureProgressSchema

---
+++ test_lms/models/courseProgress.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const lectureProgressSchema = new mongoose.Schema({
+  lecture: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Lecture",
+    required: [true, "Lecture reference is required"],
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false,
+  },
+  watchTime: {
+    type: Number,
+    default: 0,
+  },
+  lastWatched: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const courseProgressSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User refrence is required "],
+    },
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Course refrence is required "],
+    },
+    isCompleted: {
+      type: Boolean,
+      default: false,
+    },
+    completionPercentage: {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 100,
+    },
+    lectureProgress: [lectureProgressSchema],
+    lastAccessed: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
